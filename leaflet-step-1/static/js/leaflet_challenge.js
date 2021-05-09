92d78a31168e72669a7b65c2a9ba174acc801376
MaryColasanto <colasanto.mary@gmail.com>
2021-05-09 16:49:01 -0500

    started with the code from geojson as starter

--- leaflet-step-1/static/js/leaflet_challenge.js
+++ leaflet-step-1/static/js/leaflet_challenge.js
@@ -1,73 +1,8 @@
-// // Create a map object.
-// var map = L.map("map", {
-//     center: [25.5994, -45.6731],
-//     zoom: 3
-//   });
-  
-//   // Add a tile layer.
-//   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//   }).addTo(map);
-  
-//   // USGS data
-//   var earthquakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-
-//   // Loop through the cities array, and create one marker for each city object.
-//   d3.json(earthquakes).then(function(response) {
-
-//     for (var i = 0; i < response.length; i++) {
-//         var location = response[i].geometry
-//         // var depth = response[i].geometry.coordinates[3]
-
-//         console.log(location)
-//     }
-//     })  
-
-        // if (location) {
-        //     L.marker([location.coordinates[1], location.coordinates[0]]).addTo(map);
-        //   }
-        // }
-      
-
-  
-    //     // Conditionals for depth of earthquake
-    //     var color = "";
-    //     if (depth > 90) {
-    //     color = "red";
-    //     }
-    //     else if (depth > 70) && (depth <= 90) {
-    //     color = "darkorange";
-    //     }
-    //     else if (depth > 50) && (depth <= 70) {
-    //     color = "orange";
-    //     }
-    //     else if (depth > 30) && (depth <= 50) {
-    //     color = "dark yellow";
-    //     }
-    //     else if (depth > 10) && (depth <= 30) {
-    //     color = "yellowgreen";
-    //     }
-    //     else {
-    //     color = "green";
-    //     }
-  
-    //     // Add a circle for each earthquake
-    //     L.circle(location.coordinates[1], location.coordinates[0], {
-    //     fillOpacity: 0.75,
-    //     color: "white",
-    //     fillColor: color,
-    //     // Adjust the radius.
-    //     radius: depth * 1500
-    //     }).bindPopup("<h1>" + countries[i].name + "</h1> <hr> <h3>Points: " + countries[i].points + "</h3>").addTo(map);
-    // }
-  // });
-
-
-  // Store our API endpoint as queryUrl.
-var USGSUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+// Store our API endpoint as queryUrl.
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Perform a GET request to the query URL/
-d3.json(USGSUrl).then(function (data) {
+d3.json(queryUrl).then(function (data) {
   // Once we get a response, send the data.features object to the createFeatures function.
   createFeatures(data.features);
 });
@@ -77,45 +12,6 @@ function createFeatures(earthquakeData) {
   // Define a function that we want to run once for each feature in the features array.
   // Give each feature a popup that describes the place and time of the earthquake.
   function onEachFeature(feature, layer) {
-    var color = "";
-        if (depth > 90) {
-        color = "red";
-        }
-        else if (depth > 70) && (depth <= 90) {
-        color = "darkorange";
-        }
-        else if (depth > 50) && (depth <= 70) {
-        color = "orange";
-        }
-        else if (depth > 30) && (depth <= 50) {
-        color = "dark yellow";
-        }
-        else if (depth > 10) && (depth <= 30) {
-        color = "yellowgreen";
-        }
-        else {
-        color = "green";
-        }
-  
-        // Add a circle for each earthquake
-        L.circle(location.coordinates[1], location.coordinates[0], {
-        fillOpacity: 0.75,
-        color: "white",
-        fillColor: color,
-        // Adjust the radius.
-        radius: depth * 1500
-        }).bindPopup("<h1>" + countries[i].name + "</h1> <hr> <h3>Points: " + countries[i].points + "</h3>").addTo(map);
-    }
-  });
-    
-    
-    
-    
-    
-    
-    
-    
-    
     layer.bindPopup("<h3>" + feature.properties.place +
       "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
   }
@@ -153,16 +49,19 @@ function createMap(earthquakes) {
   };
 
   // Create our map, giving it the streetmap and earthquakes layers to display on load.
-  var map = L.map("map", {
+  var myMap = L.map("map", {
     center: [
-      37.09, -45.71
+      37.09, -95.71
     ],
-    zoom: 2,
+    zoom: 5,
     layers: [street, earthquakes]
   });
 
-  // Layer control.
+  // Create a layer control.
+  // Pass it our baseMaps and overlayMaps.
+  // Add the layer control to the map.
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
-  }).addTo(map);
+  }).addTo(myMap);
+
 }
